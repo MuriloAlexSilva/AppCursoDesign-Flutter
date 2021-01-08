d2c6180dac0f3bb0dd29e3f502ad9b05fc1f374d
Murilo Silva <murilo.alex.silva@gmail.com>
2021-01-08 10:44:13 -0300

    adequações

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'LoginWidget.dart';
 
 void main() {
--- lib/pages/LoginPageWidget.dart
+++ lib/pages/LoginPageWidget.dart
@@ -9,47 +9,49 @@ class LoginPageWidget extends StatefulWidget {
 class _LoginPageWidgetState extends State<LoginPageWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: 40), //Coloca um tamanho simetrico nos widgets
-      color: Theme.of(context)
-          .accentColor
-          .withOpacity(0.9), //Para colocar a opacidade
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            Icons.headset_mic,
-            color: Colors.white,
-            size: 40,
-          ),
-          Container(height: 30),
-          RichText(
-            /*Seria para concatenarmos varios textos e realizarmos designs diferentes*/
-            text: TextSpan(
-              style: TextStyle(fontSize: 20),
-              children: <TextSpan>[
-                TextSpan(text: "Awesome"),
-                TextSpan(
-                  text: "App",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: 40), //Coloca um tamanho simetrico nos widgets
+        color: Theme.of(context)
+            .accentColor
+            .withOpacity(0.9), //Para colocar a opacidade
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.headset_mic,
+              color: Colors.white,
+              size: 40,
             ),
-          ),
-          Container(height: 120),
-          CircleButton(
-            backgroundColor: Colors.transparent,
-            borderColor: Colors.white,
-            label: "Sign Up",
-          ),
-          Container(height: 30),
-          CircleButton(
-            label: "Login",
-            backgroundColor: Colors.white,
-            textColor: Theme.of(context).accentColor,
-          ),
-        ],
+            Container(height: 30),
+            RichText(
+              /*Seria para concatenarmos varios textos e realizarmos designs diferentes*/
+              text: TextSpan(
+                style: TextStyle(fontSize: 20),
+                children: <TextSpan>[
+                  TextSpan(text: "Awesome"),
+                  TextSpan(
+                    text: "App",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Container(height: 120),
+            CircleButton(
+              backgroundColor: Colors.transparent,
+              borderColor: Colors.white,
+              label: "Sign Up",
+            ),
+            Container(height: 30),
+            CircleButton(
+              label: "Login",
+              backgroundColor: Colors.white,
+              textColor: Theme.of(context).accentColor,
+            ),
+          ],
+        ),
       ),
     );
   }
